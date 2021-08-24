443dc862c6202704f50b6da745447c9af30ffe94
Zenacia <17864213831@163.com>
2021-08-24 16:03:56 +0800

    fix farm apr

--- src/views/Farms/hooks/useKacoPerBlock.ts
+++ src/views/Farms/hooks/useKacoPerBlock.ts
@@ -1,8 +1,11 @@
+import { BIG_TEN } from './../../../utils/bigNumber';
 import RealBigNumber from 'bignumber.js';
 import { BigNumber } from '@ethersproject/bignumber';
 
 import { useEffect, useState } from 'react';
 import { useMasterchef } from 'hooks/useContract';
+
+const base = BIG_TEN.pow(new RealBigNumber(18));
 
 const useKacPerBlock = (): RealBigNumber => {
   const masterChefContract = useMasterchef();
@@ -10,7 +13,7 @@ const useKacPerBlock = (): RealBigNumber => {
 
   useEffect(() => {
     masterChefContract.cakePerBlock().then(
-      (kacPerBlock: BigNumber) => setKacPerBlock(new RealBigNumber(kacPerBlock.toString())),
+      (kacPerBlock: BigNumber) => setKacPerBlock(new RealBigNumber(kacPerBlock.toString()).div(base)),
       (e) => console.error(e),
     );
   }, [masterChefContract]);
