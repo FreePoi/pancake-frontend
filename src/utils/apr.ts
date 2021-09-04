a20e5cd7f7ea39d0d84c862fb1ec520e536deaf3
suiyu <xiaohan.shixh@alibaba-inc.com>
2021-09-04 10:08:05 +0800

    feat -> add POLS-DOT farm

--- src/utils/apr.ts
+++ src/utils/apr.ts
@@ -40,7 +40,7 @@ export const getFarmApr = (
   const BLOCKS_PER_DAY = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24);
   const daylyKacRewardAllocation = kacPerBlock.times(BLOCKS_PER_DAY).times(poolWeight);
   const kacRewardsDaylyApr = daylyKacRewardAllocation.times(kacPriceUsd).div(poolLiquidityUsd).toNumber();
-  const kacRewardsApy = new BigNumber((kacRewardsDaylyApr + 1) ** 365 * 100);
+  const kacRewardsApy = new BigNumber(kacRewardsDaylyApr + 1).pow(365).multipliedBy(100);
 
   const yearlyKacRewardAllocation = kacPerBlock.times(BLOCKS_PER_YEAR).times(poolWeight);
   const kacRewardsApr = yearlyKacRewardAllocation.times(kacPriceUsd).div(poolLiquidityUsd).times(100);
