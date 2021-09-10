1113e20e868e4cb434927ebe4033395a310acb2c
Zenacia <17864213831@163.com>
2021-09-10 22:53:52 +0800

    Temporarily remove fontfamily

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -13,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Microsoft sYaHei';
     font-style: normal;
     font-weight: 400;
-    src: url(/fonts/ms-yahei.ttf);
+    src: url(/fonts-NO/ms-yahei.ttf);
   }
   * {
     font-family: Microsoft sYaHei;
